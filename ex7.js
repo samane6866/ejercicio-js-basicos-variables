834bf867189cf915a35c97618eaae4f162d46974
Oscar Miras <oscar.miras@gmail.com>
2023-04-10 12:21:18 +0200

    Create ex7.js

---
+++ ex7.js
@@ -0,0 +1,19 @@
+/** Modifica la línea correspondiente para crear un string que use todas las variables */
+
+/** Mi gato se llama Michi, tiene 3 años y pesa 4.5 kilos. Es de color blanco y negro y tiene una personalidad juguetón y curioso. 
+Le encanta comer atún y tiene 5 juguetes diferentes. Necesita dormir al menos 16 horas al día. 
+Ahora mismo está jugando con su pelota. ¡Es un gato muy feliz!
+*/
+
+let nombreGato = "Michi";
+let edadGato = 3;
+let pesoGato = 4.5;
+let colorGato = "blanco y negro";
+let personalidadGato = "juguetón y curioso";
+let comidaFavorita = "atún";
+let numJuguetes = 5;
+let numHorasSueño = 16;
+let jugandoAhora = "pelota";
+
+let historiaGato; // modificar solamente  esta línea
+console.log(historiaGato);
