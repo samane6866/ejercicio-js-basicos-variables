7cdac312e602882ff9f3002c1ed759da25987261
Oscar Miras <oscar.miras@gmail.com>
2023-04-10 12:36:53 +0200

    Create ex8.js

---
+++ ex8.js
@@ -0,0 +1,33 @@
+/** Completo cada console.log vació con el código necesario para realizar cada ejercicio. Ayuda: https://www.w3schools.com/js/js_string_methods.asp  */
+
+let texto = "singulars"; // Si cambiaramos esta palabra, debería seguir funcionando todos los ejercicios
+
+console.log("Ejercicio 1: Longitud del string"); 
+console.log();  // por ejemplo, aquí, entre los paréntesis, tenemos que poner: texto.length
+
+console.log("Ejercicio 2: Primer carácter del string"); 
+console.log(); 
+
+console.log("Ejercicio 3: Último carácter del string"); 
+console.log(); 
+
+console.log("Ejercicio 4: Primeros 3 caracteres del string"); 
+console.log(); 
+
+console.log("Ejercicio 5: Últimos 3 caracteres del string"); 
+console.log(); 
+
+console.log("Ejercicio 6: String en mayúsculas"); 
+console.log(); 
+
+console.log("Ejercicio 7: String en minúsculas"); 
+console.log(); 
+
+console.log("Ejercicio 8: Devuelve el índice del primer carácter 's'"); 
+console.log(); 
+
+console.log("Ejercicio 9: Posición del último carácter 's'"); 
+console.log(); 
+
+console.log("Ejercicio 10: String en orden inverso"); 
+console.log(); 
